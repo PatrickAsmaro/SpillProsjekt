c7f367c8e223da60c6e0d5b92dc94eb8321c41eb
PatrickAsmaro <Patrickasm@bfk.no>
2026-04-28 12:33:07 +0200

    Update hoppespill.js

--- JavaScript/hoppespill.js
+++ JavaScript/hoppespill.js
@@ -1,24 +1,28 @@
 //#region Globale Variabler
+let karakter1 ={
+  bildet1 : "bildetSti1",
+  evne1 : "evne"
+};
 
 //#endregion
 
 //#region Elementer
-let startKnapp = document.getElementById("startKnapp");
-let startMeny = document.getElementById("startSide");
+let startKnappEl = document.getElementById("startKnapp");
+let startMenyEl = document.getElementById("startSide");
 
 //#endregion
 
 
 //#region DOM
-startKnapp.addEventListener("click",startSpill);
+startKnappEl.addEventListener("click",startSpill);
 
 //#endregion
 
 
 //#region Funksjoner
 function startSpill(){    //Funksjon som starter spillet. Ingen parametre
-  startMeny.style.display = "none"
-}
+  startMeny.style.display = "none";
+};
 
 
 //#endregion

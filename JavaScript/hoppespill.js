23bcb3b8d13e05dcfcc038dc0374cdc983e3347b
PatrickAsmaro <Patrickasm@bfk.no>
2026-05-12 12:40:56 +0200

    Update hoppespill.js

--- JavaScript/hoppespill.js
+++ JavaScript/hoppespill.js
@@ -66,9 +66,22 @@ function visDiv(obj) {
   }
 }
 
+/**
+ * Funksjonen hopp() starter et hopp når mellomromstasten trykkes.
+ * 
+ * Betingelser:
+ * - event.code må være "Space" (mellomromstasten)
+ * - paaBakken må være true (spilleren står på bakken)
+ * 
+ * Endrer:
+ * - fartY til 15 (gir spilleren fart oppover)
+ * - paaBakken til false (spilleren er i lufta)
+ * 
+ * @param {KeyboardEvent} event - Tastetrykk-hendelsen fra nettleseren
+ */
 function hopp(event) {
   if (event.code === "Space" && paaBakken) {
-    event.preventDefault();
+    event.preventDefault();  // Hindrer at siden scroller
     fartY = 15;
     paaBakken = false;
   }

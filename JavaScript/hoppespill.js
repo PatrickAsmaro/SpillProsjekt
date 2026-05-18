6c0e42b2906db86f7ac636a4448767ffb92b2775
PatrickAsmaro <Patrickasm@bfk.no>
2026-05-18 14:12:17 +0200

    Update hoppespill.js

--- JavaScript/hoppespill.js
+++ JavaScript/hoppespill.js
@@ -3,67 +3,61 @@ let y = 0;
 let fartY = 0;
 let paaBakken = true;
 let aktivKarakter;
+let fienderAktiv = [];
 
-//Spillbrett
-let brett;
+let bakgrunn = {
+  1: "../Bilder/bakgrunn1.jpg",
+  2: "../Bilder/bakgrunn2.jpg",
+  3: "../Bilder/bakgrunn3.jpg"
+};
+
+// Spillbrett
 let brettBredde = 750;
 let brettHoyde = 250;
 
-//Spillkarakter
+// Spillkarakter
 let karakterBredde = 80;
 let karakterHoyde = 86;
 let karakterX = 50;
-//let karakterY = 0; feil
-
-let karakterImg;
 
 let karakterer = [
-  { navn: "test", evne: "evne1", liv: 3, bilde: "../Bilder/testKarakter.jpg" },
-  { navn: "karakter2", evne: "evne2", liv: 3, bilde: "bildeSti" },
-  { navn: "karakter3", evne: "evne3", liv: 3, bilde: "bildeSti" }
+  { navn: "test",       evne: "evne1", liv: 3, bilde: "../Bilder/testKarakter.jpg" },
+  { navn: "karakter2",  evne: "evne2", liv: 3, bilde: "bildeSti" },
+  { navn: "karakter3",  evne: "evne3", liv: 3, bilde: "bildeSti" }
 ];
 
-//fiender
-let fiende1Bredde = 34
-let fiende1Img;
-
-let fiende2Bredde = 69
-let fiende2Img;
-
-let fiende3Bredde = 102
-let fiende3Img;
-
-
-let fiendeHoyde = 70
-
-let fiendeX = 700
-let fiendeY = brettHoyde - fiendeHoydeks
-
-let fienderArray = [
-  {nr : "1", bredde: 34, hoyde: 70, x: fiendeX, y: fiendeY, img: "../Bilder/fiende1.png"},
-  {nr : "2", bredde: 69, hoyde: 70, x: fiendeX, y: fiendeY, img: "../Bilder/fiende2.png"},
-  {nr : "3", bredde: 102, hoyde: 70, x: fiendeX, y: fiendeY, img: "../Bilder/fiende3.png"}
+// Fiende-typer (maler, skal ikke endres under spill)
+const fiendeMaler = [
+  { bredde: 34,  hoyde: 70, img: "../Bilder/fiende1.png" }, // liten  – 60%
+  { bredde: 69,  hoyde: 70, img: "../Bilder/fiende2.png" }, // medium – 30%
+  { bredde: 102, hoyde: 70, img: "../Bilder/fiende3.png" }  // stor   – 10%
 ];
+
+const fiendeStartX = brettBredde - 50;
+const fiendeStartY = brettHoyde - 70;
 //#endregion
+
 
 //#region Elementer
-let bodyEl = document.querySelector("body");
+let bodyEl      = document.querySelector("body");
 let startKnappEl = document.querySelector("#startKnapp");
-let startMenyEl = document.querySelector("#startSide");
+let startMenyEl  = document.querySelector("#startSide");
 //#endregion
+
 
 //#region DOM
 startKnappEl.addEventListener("click", startSpill);
 //#endregion
 
+
 //#region Funksjoner
+
 /**
- * Starter spillet ved å skjule startmenyen, legge til en keydown-event listener for hopp, koble bilder til karakter-divs, vise den første karakteren og starte spilloppdateringen.
- * 
+ * Starter spillet ved å skjule startmenyen, legge til keydown-lytter for hopp,
+ * koble bilder til karakter-divs, vise første karakter og starte spilløkka.
  */
 function startSpill() {
   startMenyEl.style.display = "none";
-
   document.addEventListener("keydown", hopp);
 
   for (let i = 0; i < karakterer.length; i++) {
@@ -72,32 +66,26 @@ function startSpill() {
 
   visDiv(karakterer[0]);
   aktivKarakter = document.querySelector("#" + karakterer[0].navn);
-
   oppdaterSpill();
 }
 
+
 /**
  * Kobler et bilde til et div-element basert på karakterobjektet.
- * 
- * @param {Object} obj - Karakterobjektet med egenskapen navn og bilde
- * @returns {HTMLElement} - Det opprettede div-elementet
+ * Oppretter ikke ny div hvis den allerede finnes.
+ * @param {Object} obj - Karakterobjekt med egenskapene navn og bilde
  */
 function kobleBildeTilDiv(obj) {
-  if (document.querySelector("#" + obj.navn)) {
-    return;
-  }
+  if (document.querySelector("#" + obj.navn)) return;
 
   let div = document.createElement("div");
   div.id = obj.navn;
-  div.style.display = "none";
-  div.style.position = "absolute";
-  div.style.bottom = "0px";
-  div.style.left = "100px";
+  div.style.cssText = "display:none; position:absolute; bottom:0px; left:" + karakterX + "px;";
 
   let img = document.createElement("img");
   img.src = obj.bilde;
   img.alt = obj.navn;
-  img.style.width = "100px";
+  img.style.width = karakterBredde + "px";
 
   div.appendChild(img);
   bodyEl.appendChild(div);
@@ -105,56 +93,38 @@ function kobleBildeTilDiv(obj) {
 
 
 /**
- * Viser en div-element på skjermen basert på karakterobjektet.
- * 
- * Betingelser:
- * - obj.navn må eksistere (karakteren må ha et navn)
- * - Div-elementet med id lik obj.navn må eksistere i DOM
- * 
- * Endrer:
- * - style.display til "block" (gjør elementet synlig)
- * 
- * @param {Object} obj - Karakterobjektet med egenskapen navn
+ * Gjør div-elementet til obj synlig.
+ * @param {Object} obj - Karakterobjekt med egenskapen navn
  */
 function visDiv(obj) {
   let divEl = document.querySelector("#" + obj.navn);
-
   if (divEl) {
     divEl.style.display = "block";
   }
 }
 
+
 /**
- * Funksjonen hopp() starter et hopp når mellomromstasten trykkes.
- * 
- * Betingelser:
- * - event.code må være "Space" (mellomromstasten)
- * - paaBakken må være true (spilleren står på bakken)
- * 
- * Endrer:
- * - fartY til 15 (gir spilleren fart oppover)
- * - paaBakken til false (spilleren er i lufta)
- * 
+ * Starter et hopp når mellomromstasten trykkes og karakteren er på bakken.
  * @param {KeyboardEvent} event - Tastetrykk-hendelsen fra nettleseren
  */
 function hopp(event) {
   if (event.code === "Space" && paaBakken) {
-    event.preventDefault();  // Hindrer at siden scroller
+    event.preventDefault();
     fartY = 15;
     paaBakken = false;
   }
 }
 
+
 /**
  * Oppdaterer spillet ved å flytte karakteren opp eller ned.
- * Funksjonen bruker fartY og y for å regne ut hopp og landing.
- * Hvis karakteren treffer bakken, settes y til 0 og paaBakken til true.
- * Flytter aktivKarakter ved å endre style.bottom.
+ * Bruker fartY og y for å beregne hopp og landing.
  * Returnerer ingen verdi.
  */
 function oppdaterSpill() {
-  fartY = fartY - 1;
-  y = y + fartY;
+  fartY -= 1;
+  y += fartY;
 
   if (y <= 0) {
     y = 0;
@@ -169,49 +139,63 @@ function oppdaterSpill() {
   requestAnimationFrame(oppdaterSpill);
 }
 
-function lagFiender() {
+
+/**
+ * Lager en ny aktiv fiende basert på tilfeldighet og legger den til fienderAktiv.
+ * 10% sjanse: stor fiende, 30% sjanse: medium fiende, 60% sjanse: liten fiende.
+ * Fjerner den eldste fienden hvis listen overstiger 5 elementer.
+ */
+function lagFiende() {
+  let spawnSjanse = Math.floor(Math.random() * 10 + 1);
+
+  let mal;
+  if (spawnSjanse > 9) {
+    mal = fiendeMaler[2];       // 10% – stor
+  } else if (spawnSjanse >= 7) {
+    mal = fiendeMaler[1];       // 30% – medium
+  } else {
+    mal = fiendeMaler[0];       // 60% – liten
+  }
+
   let fiende = {
-      img : null,
-      x : fiendeX,
-      y : fiendeY,
-      width : null,
-      height : fiendeHoyde
-    }
-  
-  let spawnSjanse = Math.floor(Math.random * 10 + 1)
-  
-  if(spawnSjanse > 9){    //10% sjanse for en stor fiende
-    fiende.img = fiende3Img;
-    fiende.width = fiende3Bredde
-    fienderArray.push(fiende)
-  } else if (spawnSjanse >= 7){    //30% sjanse for en medium fiende
-    fiende.img = fiende2Img;
-    fiende.width = fiende2Bredde
-    fienderArray.push(fiende)
-  } else if (spawnSjanse <= 6){   //60% sjansse for en liten fiende
-    fiende.img = fiende1Img;
-    fiende.width = fiende1Bredde
-    fienderArray.push(fiende)
-  }
+    img:    mal.img,
+    x:      fiendeStartX,
+    y:      fiendeStartY,
+    width:  mal.bredde,
+    height: mal.hoyde
+  };
 
-  if(fienderArray.length > 5){
-    fienderArray.shift(); //Fjerner fienden som er forran i Arrayen, sånn at den ikke blir for stor
-  }
-};
+  fienderAktiv.push(fiende);
 
+  if (fienderAktiv.length > 5) {
+    fienderAktiv.shift();
+  }
+}
+
+
+/**
+ * Sjekker om aktivKarakter kolliderer med en fiende i fienderAktiv.
+ * Fjerner fienden og logger kollisjon hvis treff oppdages.
+ */
 function kollisjon() {
-  for(let i = 0; i < fienderArray.length; i++){
-    let fiende = fienderArray[i];
-    if(aktivKarakter && fiende.x < 150 && fiende.x + fiende.width > 100 && y < fiende.y + fiende.height){
-      // Kollisjon oppdaget, håndter skade eller spill-over her
+  let karakterVenstre = karakterX;
+  let karakterHoyre   = karakterX + karakterBredde;
+  let karakterTopp    = y + karakterHoyde;
+
+  for (let i = 0; i < fienderAktiv.length; i++) {
+    let fiende = fienderAktiv[i];
+    let treffer =
+      fiende.x < karakterHoyre &&
+      fiende.x + fiende.width > karakterVenstre &&
+      fiende.y < karakterTopp &&
+      fiende.y + fiende.height > y;
+
+    if (treffer) {
       console.log("Kollisjon med fiende!");
-      // Fjern fienden fra arrayet etter kollisjon
-      fienderArray.splice(i, 1);
-      i--; // Juster indeksen etter fjerning
+      fienderAktiv.splice(i, 1);
+      i--;
     }
   }
-};
-
-
+}
 
 //#endregion
